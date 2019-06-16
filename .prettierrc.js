e8ccc0a3400610bf17fa33c2f3566f9321131ddc
Stein Cato Blostrupmoen <esseb@esseb.com>
2019-06-16 22:43:58 +0200

    Add a prettier file so we get prettier support in editors that require a prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,4 @@
+// We're not changing any default Prettier rules, but the presence of this file
+// means editors like VSCode with a setting like `Require Prettier Config` enabled
+// will know it can safely format files using Prettier.
+module.exports = {};
