f507d5eb3163181cf60c144463a81965fb3364de
Stein Cato Blostrupmoen <esseb@esseb.com>
2019-06-22 23:55:34 +0200

    Workaround for web-ext detecting file changes too quickly

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
 const path = require("path");
+const PostCompileWebpackPlugin = require("post-compile-webpack-plugin");
+const touch = require("touch");
 
-module.exports = {
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
+module.exports = (_env, argv) => {
+  return {
+    entry: "./src/index.ts",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    output: {
+      filename: "index.js",
+      path: path.resolve(__dirname, "build")
+    },
+    plugins: [
+      new PostCompileWebpackPlugin(() => {
+        const isDev = argv && argv.mode === "development";
+
+        if (isDev) {
+          // `web-ext` reloads the extension as soon as `build/index.js`
+          // is modified, but unfortunately webpack modifies the file
+          // before it has finished building.
+          // In order to make `web-ext` detect the changes
+          // we modify the file again after a short delay.
+          // After a little testing it seems like the delay
+          // has to be > 1000ms
+          setTimeout(() => {
+            touch("build/index.js");
+          }, 1100);
+        }
+      })
     ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "build")
-  }
+  };
 };
